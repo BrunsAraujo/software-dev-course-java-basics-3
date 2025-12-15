2725aa706953b035c5e5acda9dd22c1a4f0254f4
BrunsAraujo <bruno.correadearaujo@gmail.com>
2025-12-14 20:36:07 -0600

    Homework Ready - second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,18 @@ public class Main {
     public static void main(String[] args) {
         JavaCafe javaCafe = new JavaCafe();
         javaCafe.run();
+
+            ConditionalExercises ce = new ConditionalExercises();
+
+            System.out.println(ce.lessThanFive(3));       // true
+            System.out.println(ce.getAgeGroup(15));       // teen
+            System.out.println(ce.isValidPassword("abc")); // false
+            System.out.println(ce.isValidPassword("abcdefgh")); // true
+
+            LoopExercises le = new LoopExercises();
+
+            System.out.println(le.sum(5));          // 15
+            System.out.println(le.sumUntilEven(5)); // 6
+        }
+
     }
-}
